abd43a32567f04d00dd7101ccb2822bbb934bab5
kudashev <Alexander.Kudashev@softline.com>
2024-09-06 21:59:20 +0500

    поправил текст приглашения к игре

--- src/Cli.php
+++ src/Cli.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 function greet(): string
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
 
     $userName = (string) prompt('May I have your name?');
     line("Hello, %s!", $userName);
