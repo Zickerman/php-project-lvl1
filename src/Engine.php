d5eba7ec44a8539d34387902e4825cc556393b97
kudashev <Alexander.Kudashev@softline.com>
2024-09-06 22:52:53 +0500

    переместил приветствие в движок. Заменил колбэки на стрелочные функции

--- src/Engine.php
+++ src/Engine.php
@@ -5,6 +5,16 @@ namespace Php\Project\Lvl1;
 use function cli\line;
 use function cli\prompt;
 
+function greet(): string
+{
+    line('Welcome to the Brain Games!');
+
+    $userName = (string) prompt('May I have your name?');
+    line("Hello, %s!", $userName);
+
+    return $userName;
+}
+
 function playGame(string $description, callable $getGameData, string $userName)
 {
     line($description);
